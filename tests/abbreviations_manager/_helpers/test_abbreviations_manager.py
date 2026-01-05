75d48ce215eb7733f9298e9d5aec71358bc7fea9
John Bieren <jbieren64@gmail.com>
2026-01-05 11:48:01 -0800

    Full testing of abv_man and nhd
    nhd is not currently passing these tests

--- tests/abbreviations_manager/_helpers/test_abbreviations_manager.py
+++ tests/abbreviations_manager/_helpers/test_abbreviations_manager.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
-"""Tests methods of the abbreviations_manager singleton."""
+"""Tests methods of the AbbreviationsManager singleton."""
+
+import pandas as pd
 
 from brlib._helpers.abbreviations_manager import abv_man
 
-def test_correct_abvs():
+
+def test_abv_man():
+    suite()
+
+    # re-load data from cache and test again
+    abv_man.df = pd.DataFrame()
+    assert abv_man._has_valid_cache
+    abv_man._load()
+    suite()
+
+def suite():
+    correct_abvs()
+    franchise_abv()
+    all_team_abvs()
+    to_alias()
+    to_regular()
+
+def correct_abvs():
     assert abv_man.correct_abvs("OAK", 2025, era_adjustment=True) == ["ATH"]
     assert abv_man.correct_abvs("OAK", 2025, era_adjustment=False) == []
     assert abv_man.correct_abvs("BAL", 1915, era_adjustment=True) == ["SLB", "BAL"]
@@ -13,24 +32,24 @@ def test_correct_abvs():
     assert abv_man.correct_abvs("LAA", 1907, era_adjustment=False) == []
     assert abv_man.correct_abvs("SER", 2025, era_adjustment=False) == []
 
-def test_franchise_abv():
+def franchise_abv():
     assert abv_man.franchise_abv("ATH", 1876) == "ATH"
     assert abv_man.franchise_abv("BAL", 1915) == "BLT"
     assert abv_man.franchise_abv("OAK", 2025) == ""
     assert abv_man.franchise_abv("SER", 2025) == ""
 
-def test_all_team_abvs():
+def all_team_abvs():
     assert abv_man.all_team_abvs("ATH", 2025) == ["ATH", "KCA", "OAK", "PHA"]
     assert abv_man.all_team_abvs("OAK", 2025) == []
     assert abv_man.all_team_abvs("SER", 2025) == []
 
-def test_to_alias():
+def to_alias():
     assert abv_man.to_alias("SEA", 2025) == "SEA"
     assert abv_man.to_alias("KCA", 1963) == "KC1"
     assert abv_man.to_alias("PBS", 2025) == "PBS"
     assert abv_man.to_alias("SER", 2025) == "SER"
 
-def test_to_regular():
+def to_regular():
     assert abv_man.to_regular("SEA", 2025) == "SEA"
     assert abv_man.to_regular("KCA", 1999) == "KCR"
     assert abv_man.to_regular("KC1", 2025) == "KC1"
---
+++ tests/abbreviations_manager/_helpers/test_no_hitter_dicts.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""Tests contents of the NoHitterDicts singleton."""
+
+from brlib._helpers.no_hitter_dicts import nhd
+
+
+def test_nhd():
+    assert not nhd._populated
+    nhd.populate()
+    suite()
+
+    # re-load data from cache and test again
+    nhd.game_inh_dict, nhd.game_pg_dict, nhd.game_cnh_dict = ({} for _ in range(3))
+    nhd.player_inh_dict, nhd.player_pg_dict, nhd.player_cnh_dict = ({} for _ in range(3))
+    nhd.team_inh_dict, nhd.team_pg_dict, nhd.team_cnh_dict = ({} for _ in range(3))
+    assert nhd._has_valid_cache
+    nhd._load()
+    suite()
+
+def suite():
+    game_dicts()
+    player_dicts()
+    team_dicts()
+
+def game_dicts():
+    assert nhd.game_inh_dict["CIN202408020"] == "snellbl01"
+    assert nhd.game_inh_dict["NYA195610080"] == "larsedo01"
+
+    assert nhd.game_pg_dict.get("CIN202408020") is None
+    assert nhd.game_pg_dict["NYA195610080"] == "larsedo01"
+
+    assert nhd.game_cnh_dict.get("NYA195610080") is None
+    assert nhd.game_cnh_dict["SEA201206080"] == [
+        "millwke01", "pryorst01", "furbuch01", "luetglu01", "leagubr01", "wilheto01"
+    ]
+    assert nhd.game_cnh_dict["DET202307080"] == ["mannima02", "foleyja01", "langeal01"]
+
+def player_dicts():
+    assert nhd.player_inh_dict.get("pressry01") is None
+    assert nhd.player_inh_dict["hallaro01"] == [["2010", "PHI", "P"], ["2010", "PHI", "R"]]
+    # checks that Negro League no-hitters are marked as regular season
+    assert nhd.player_inh_dict["dayle99"] == [["1946", "NE", "R"]]
+
+    assert nhd.player_pg_dict.get("paxtoja01") is None
+    assert nhd.player_pg_dict["hernafe02"] == [["2012", "SEA", "R"]]
+    assert nhd.player_pg_dict["larsedo01"] == [["1956", "NYY", "P"]]
+
+    assert nhd.player_cnh_dict.get("dayle99") is None
+    assert nhd.player_cnh_dict["sanchaa01"] == [["2019", "HOU", "R"]]
+    assert nhd.player_cnh_dict["pressry01"] == [["2022", "HOU", "P"], ["2022", "HOU", "R"]]
+
+def team_dicts():
+    assert nhd.team_inh_dict.get("LAD2018") is None
+    # checks that pre-1901 no-hitters are marked as regular season
+    assert nhd.team_inh_dict["BRO1886"] == [["terryad01", "R"]]
+    assert nhd.team_inh_dict["PHI2010"] == [["hallaro01", "P"], ["hallaro01", "R"]]
+
+    assert nhd.team_pg_dict.get("BRO1886") is None
+    assert nhd.team_pg_dict["CHW2012"] == [["humbeph01", "R"]]
+    assert nhd.team_pg_dict["NYY1956"] == [["larsedo01", "P"]]
+
+    assert nhd.team_cnh_dict.get("SEA2018") is None
+    assert nhd.team_cnh_dict["LAD2018"] == [
+        ["buehlwa01", "R", "SDN201805040"], ["cingrto01", "R", "SDN201805040"],
+        ["garciyi01", "R", "SDN201805040"], ["liberad01", "R", "SDN201805040"]
+    ]
+    assert nhd.game_cnh_dict["HOU2022"] == [
+        ["javiecr01", "P", "PHI202211020"], ["abreubr01", "P", "PHI202211020"],
+        ["montera01", "P", "PHI202211020"], ["pressry01", "P", "PHI202211020"],
+        ["javiecr01", "R", "NYA202206250"], ["nerishe01", "R", "NYA202206250"],
+        ["pressry01", "R", "NYA202206250"]
+    ]
